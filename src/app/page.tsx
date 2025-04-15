ad9e25aede05e23f48e85c90311bfa1fca7392ee
elmar.amanov <elmar.amanov@cybernet.az>
2025-04-15 23:27:23 +0400

    feat: syntax error  leading decorators must be attached to a class declaration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { fontCalSans } from './ui/fonts/fontCalSans';
 
+const text = 'Hello World';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p className={fontCalSans.variable}>
-        First Lesson First Lesson w First Lesson w First Lesson w hfjhjhcjh
-      </p>
+      <p className={fontCalSans.variable}>{text}</p>
     </div>
   );
 }
