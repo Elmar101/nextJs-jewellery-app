7d77788ba13bcd9beb75c3cf3facdfea42affb62
Elmar Alibeyli <65470729+Elmar101@users.noreply.github.com>
2025-05-15 06:34:06 +0400

    Update route.ts

--- src/app/(users)/api/blog/route.ts
+++ src/app/(users)/api/blog/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 import { blogs } from './data';
 
-export const GET = async (req: NextRequest): Promise<Response> => {
+export const GET = async (): Promise<Response> => {
     return new Response(JSON.stringify( blogs), { status: 200, headers: { 'Content-Type': 'application/json' } });
 };
